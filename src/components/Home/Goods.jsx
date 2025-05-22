8203f43103fe1d4df975ef7d41b4d358b90b1d5f
abdelaty7 <muhvmmvdessvm2@gmail.com>
2025-05-22 14:18:54 +0300

    first commit

--- src/components/Home/Goods.jsx
+++ src/components/Home/Goods.jsx
@@ -17,15 +17,15 @@ const Goods = () => {
       </div>
     </div>
     <div className='flex flex-col items-center sm:flex-row gap-3 sm:gap-8 mx-7 sm:mx-15 justify-center pb-10'>
-      <div className='bg-white border-b-3 sm:border-b-5 border-amber-400 w-full sm:w-83 rounded-xl p-3 sm:p-6 flex flex-col items-center'>
+      <div className='bg-white h-65 border-b-3 sm:border-b-5 border-amber-400 w-full sm:w-83 rounded-xl p-3 sm:p-6 flex flex-col items-center'>
         <p className='text-[15.8px] sm:text-[17px] font-semibold text-indigo-950 mt-1 sm:mt-0 mb-2 sm:mb-4'>جودة مضمونة</p>
         <p className='font-medium text-[12.5px] sm:text-[14px] text-gray-700 tracking-wider mb-2 sm:mb-0'>في أولاد شوقي، بنؤمن إن الجودة هي الأساس في كل حاجة بنقدمها. علشان كده كل منتجاتنا مختارة بعناية، ومعتمدة ومطابقة للمواصفات القياسية المصرية والعالمية. إحنا مش بس بنوفرلك منتج بيؤدي الغرض، إحنا بنضمنلك إنه يعيش معاك فترة طويلة بأعلى درجات الأمان والكفاءة، سواء كنت بتجهز بيتك أو بتجدد مكانك.</p>
       </div>
-      <div className='bg-white border-b-3 sm:border-b-5 border-amber-400 w-full sm:w-83 rounded-xl p-3 sm:p-6 flex flex-col items-center'>
+      <div className='bg-white h-65 border-b-3 sm:border-b-5 border-amber-400 w-full sm:w-83 rounded-xl p-3 sm:p-6 flex flex-col items-center'>
         <p className='text-[15.8px] sm:text-[17px] font-semibold text-indigo-950 mt-1 sm:mt-0 mb-2 sm:mb-4'>أسعار تنافسية</p>
         <p className='font-medium text-[12.5px] sm:text-[14px] text-gray-700 tracking-wider mb-2 sm:mb-0'>عارفين كويس إن تجهيز أو تجديد بيتك بيحتاج تخطيط كويس للميزانية، وعشان كده إحنا حرصنا نوفرلك أفضل قيمة مقابل السعر. في أولاد شوقي، مش بس بتاخد منتج عالي الجودة، لكن كمان بتحصل عليه بسعر مناسب يناسب كل الإمكانيات بدون ما تضطر تتنازل عن احتياجاتك أو عن الذوق اللي بتحلم بيه.</p>
       </div>
-      <div className='bg-white border-b-3 sm:border-b-5 border-amber-400 w-full sm:w-83 rounded-xl p-3 sm:p-6 flex flex-col items-center'>
+      <div className='bg-white h-65 border-b-3 sm:border-b-5 border-amber-400 w-full sm:w-83 rounded-xl p-3 sm:p-6 flex flex-col items-center'>
         <p className='text-[15.8px] sm:text-[17px] font-semibold text-indigo-950 mt-1 sm:mt-0 mb-2 sm:mb-4'>توافر دائم للبضاعة</p>
         <p className='font-medium text-[12.5px] sm:text-[14px] text-gray-700 tracking-wider mb-2 sm:mb-0'>مفيش حاجة بتضايق أكتر من إنك تدور على حاجة وتلاقيها مش موجودة. عشان كده إحنا بنحرص دايمًا على توفر مخزون كامل ومتجدد من كل مستلزمات الكهرباء. سواء احتجت سلك معين، بريزة، كشاف إضاءة، أو حتى لمبة مودرن، هتلاقيه متوفر في أي وقت تزورنا فيه، وجاهزين نخدمك بسرعة ومن غير انتظار.</p>
       </div>
--- src/components/Utils/Footer.jsx
+++ src/components/Utils/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
 
   return (
       <footer className="bg-black text-gray-300 text-center py-3.5 sm:p-3 text-[12.5px]">
-        © {currentYear} أولاد شوقي. جميع الحقوق محفوظة.
+        © {currentYear} أولاد شوقي. جميع الحقوق محفوظة. <a href="https://www.linkedin.com/in/abdel2ty/" className='opacity-75'>حساب المطور</a>
       </footer>
   );
 };
